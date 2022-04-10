cb46c2ae73d1359090e8f53996836bbde49e7b5b
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-04-10 17:38:20 -0300

    Add daily event API

--- calendar_api/models.py
+++ calendar_api/models.py
@@ -9,7 +9,7 @@ class WeeklyAcademicCalendar(models.Model):
     week_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     week_alias = models.CharField(max_length=255)
     year = models.IntegerField()
-    week_start = models.DateField()
+    # week_start = models.DateField()
     week_observations = models.CharField(max_length=255, blank=True, null=True)
     week_regular_typical_activities = models.CharField(max_length=255, blank=True, null=True)
     week_project_typical_activities = models.CharField(max_length=255, blank=True, null=True)
